a18be48964f627116dd7d43c4216de40cabb5f83
tanin9 <taninnusrat199@gmail.com>
2026-04-15 01:23:32 +0600

    navbar and footer added

--- src/components/shared/footer/Footer.jsx
+++ src/components/shared/footer/Footer.jsx
@@ -2,9 +2,55 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>footer</h2>
-        </div>
+      <div className="container mx-auto bg-forest-green py-6">
+        <footer className="footer footer-horizontal footer-center text-white p-10">
+          <aside>
+            <p className="font-bold text-[4.4vh]">KeenKeeper</p>
+            <p className="text-[1.4vh]">
+              Your personal shelf of meaningful connections. Browse, tend, and
+              nurture the relationships that matter most.
+            </p>
+          </aside>
+          <nav>
+            <div>
+              <p className='text-xl'>Social Links</p>
+            </div>
+            <div className="grid grid-flow-col gap-4">
+              <a>
+                <img
+                  src="/src/assets/images/instagram.png"
+                  alt=""
+                />
+              </a>
+              <a>
+                <img
+                  src="/src/assets/images/facebook.png"
+                  alt=""
+                />
+              </a>
+              <a>
+                {" "}
+                <img
+                  src="/src/assets/images/twitter.png"
+                  alt=""
+                />
+              </a>
+            </div>
+          </nav>
+        </footer>
+        <footer className="footer  text-[#59786d] border-[#59786d]  px-10 py-4">
+          <div className="navbar border-t flex flex-col lg:flex-row lg:justify-between lg:items-center gap-4 pt-4">
+            <div>
+              <p>© {new Date().getFullYear()} Copyright. All right reserved.</p>
+            </div>
+            <div className="grid sm:grid-col md:flex gap-4 mr-3">
+              <a className="link link-hover">Terms of use</a>
+              <a className="link link-hover">Privacy policy</a>
+              <a className="link link-hover">Cookie policy</a>
+            </div>
+          </div>
+        </footer>
+      </div>
     );
 };
 
--- src/components/shared/navbar/Navbar.jsx
+++ src/components/shared/navbar/Navbar.jsx
@@ -1,10 +1,88 @@
 import React from 'react';
+import { RiHome2Line } from "react-icons/ri";
+import { LuClock3 } from "react-icons/lu";
+import { VscGraphLine } from "react-icons/vsc";
 
 const Navbar = () => {
     return (
-        <div>
-         <h2>navbar</h2>
+      <div>
+        <div className="navbar bg-base-100 shadow-sm">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  {" "}
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />{" "}
+                </svg>
+              </div>
+              <ul
+                tabIndex="-1"
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              >
+                <li>
+                  <a>
+                    <RiHome2Line />
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    <LuClock3 />
+                    Timeline
+                  </a>
+                </li>
+                <li>
+                  <a> 
+                    <VscGraphLine />
+                    Stats
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <a className="btn btn-ghost text-xl  ">
+              Keen<span className="forest-green">Keeper</span>
+            </a>
+          </div>
+          <div className="navbar-end hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+              <li>
+                <a>
+                  <RiHome2Line />
+                  Home
+                </a>
+              </li>
+              <li>
+                <a>
+                  <LuClock3 />
+                  Timeline
+                </a>
+              </li>
+              <li>
+                <a>
+                  <VscGraphLine />
+                  Stats
+                </a>
+              </li>
+            </ul>
+          </div>
+         
         </div>
+      </div>
     );
 };
 
