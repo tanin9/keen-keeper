46ed1bc38a6faa5e8c1e1cfb012ca3242bf0cfcb
tanin9 <taninnusrat199@gmail.com>
2026-04-14 23:18:43 +0600

    basic components are added

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/shared/navbar/Navbar';
+import { Outlet } from 'react-router';
+import Footer from '../components/shared/footer/Footer';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Navbar />
+            <Outlet />
+            <Footer/>
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import './index.css'
+import App from './App.jsx'
+import MainLayout from './layout/MainLayout.jsx';
+import Homepage from './pages/homepage/Homepage.jsx';
+import Friends from './pages/friends/Friends.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        element: <Homepage/>,
+      },
+      {
+        path: "/friends",
+        element:<Friends/>,
+      }
+    ]
+  },
+ 
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+    <App />
+  </StrictMode>,
+);
